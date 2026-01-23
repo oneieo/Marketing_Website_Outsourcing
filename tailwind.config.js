c23b3b01768dba656956f87f0af51f3d54bec49f
oneieo <sun020716@gmail.com>
2026-01-24 02:32:26 +0900

    add: KoPub 바탕체, Pretendard 폰트 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'batang': ['KoPubWorld Batang', 'serif'],
+        'pretendard': ['Pretendard', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
